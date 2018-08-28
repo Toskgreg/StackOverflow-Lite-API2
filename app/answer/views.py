49c81ef7b5ffddc10841c233e9a2ff8cdc92bf4f
Toskgreg <toskingregory@icloud.com>
2018-08-28 20:17:11 +0300

    [#160097705] adding answer post url route using blueprints.

--- app/answer/views.py
+++ app/answer/views.py
@@ -1,9 +1,11 @@
 """This module handle the registering of the ANSWER_APP blue_print and
  adding the adds the url rule"""
+"""Handle endpoints for the ANSWER_app blue prints """
 from flask import Blueprint
 from app.answer.api import AnswerAPI
+
 ANSWER_APP = Blueprint('ANSWER_APP', __name__)
 
 ANSWER_VIEW = AnswerAPI.as_view('answer_api')
-ANSWER_APP.add_url_rule('/api/v1/questions/<question_id>/answer/',
-                        view_func=ANSWER_VIEW, methods=['POST', ])
+ANSWER_APP.add_url_rule('/api/v2/questions/<question_id>/answers',
+                         view_func=ANSWER_VIEW, methods=['POST', ])

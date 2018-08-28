0d5c8757d8f1e169dd7895f98e35686844b025f2
Toskgreg <toskingregory@icloud.com>
2018-08-28 20:03:51 +0300

    [#160097960] registering more question blueprints

--- app/question/views.py
+++ app/question/views.py
@@ -8,8 +8,10 @@ QUESTION_VIEW = QuestionAPI.as_view('question_api')
 
 QUESTION_APP.add_url_rule('/api/v2/questions/', defaults={'question_id': None},
                       view_func=QUESTION_VIEW, methods=['GET', ])
-QUESTION_APP.add_url_rule('/api/v2/users/questions/', view_func=QUESTION_VIEW,
+QUESTION_APP.add_url_rule('/api/v2/questions/', view_func=QUESTION_VIEW,
                       methods=['POST', ])
 QUESTION_APP.add_url_rule('/api/v2/questions/<int:question_id>', view_func=QUESTION_VIEW,
                       methods=['GET', ])
+QUESTION_APP.add_url_rule('/api/v2/questions/<int:question_id>', view_func=QUESTION_VIEW,
+                      methods=['DELETE', ])
 

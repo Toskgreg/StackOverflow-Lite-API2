07126cec30f69617d873900c3e9346641d159647
Toskgreg <toskingregory@icloud.com>
2018-08-28 20:20:25 +0300

    [#160097705] adding answer put methodview to answer api file

--- app/answer/api.py
+++ app/answer/api.py
@@ -31,3 +31,43 @@ class AnswerAPI(MethodView):
         else:
             return jsonify(
                 {'message': "You can't answer your own question"}), 403
+
+    def put(self, current_user, question_id, answer_id):
+        """Accept an answer or update it."""
+        database = Database(app.config['DATABASE_URL'])
+        answer_db = AnswerBbQueries()
+        # first check if the question was created by the logged in author
+        qauthor = current_user.username
+        aauthor = current_user.username
+        query = database.fetch_by_param('questions', 'id', question_id)
+        query1 = database.fetch_by_param('answers', 'id', answer_id)
+        if query is None:
+            abort(404)
+        question = Question(query[0], query[1], query[2], query[3], query[4])
+        if question.qauthor == qauthor:
+            data = request.get_json()
+            print(data['status'])
+            if data['status'] == 'accepted' or data['status'] == 'rejected':
+
+                answer_db.update_answer_status(answer_id, data)
+                response = {
+                    'message': 'you have {} this answer'
+                    .format(data['status'])
+                }
+                return make_response(jsonify(response)), 200
+
+            response = {
+                'message': 'The status can only be in 3 states,' +
+                'answered, accepted and rejected'
+            }
+            return make_response(jsonify(response)), 406
+
+        query1 = database.fetch_by_param('answers', 'id', answer_id)
+        if query1 is None:
+            abort(404)
+        answer = Answer(query1[0], query1[1], query1[2], query1[3], query1[4])
+        if answer.aauthor ==aauthor:
+            data = request.get_json()
+            text1 = data['text1']
+            answer_db.update_answer(answer_id, text1)
+            return jsonify ({"message":"You have updated your answer"}),201

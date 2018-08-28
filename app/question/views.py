d367828af488eda529efe3c285d8e3bf37a0948f
Toskgreg <toskingregory@icloud.com>
2018-08-28 20:02:34 +0300

    [#160097960] registering question blueprints

--- app/question/views.py
+++ app/question/views.py
@@ -1,18 +1,15 @@
+"""Handles adding endponts to the blueprints"""
 from flask import Blueprint
-from app.question.api import QUESTIONAPI
+from app.question.api import QuestionAPI
 
 QUESTION_APP = Blueprint('QUESTION_APP', __name__)
 
-QUESTION_VIEW = QUESTIONAPI.as_view('question_api')
-QUESTION_APP.add_url_rule('/api/v1/questions/', defaults={'question_id': None},
-                          view_func=QUESTION_VIEW, methods=['GET', ])
+QUESTION_VIEW = QuestionAPI.as_view('question_api')
 
-QUESTION_APP.add_url_rule(
-    '/api/v1/questions/<question_id>',
-    view_func=QUESTION_VIEW,
-    methods=[
-        'GET',
-    ])
+QUESTION_APP.add_url_rule('/api/v2/questions/', defaults={'question_id': None},
+                      view_func=QUESTION_VIEW, methods=['GET', ])
+QUESTION_APP.add_url_rule('/api/v2/users/questions/', view_func=QUESTION_VIEW,
+                      methods=['POST', ])
+QUESTION_APP.add_url_rule('/api/v2/questions/<int:question_id>', view_func=QUESTION_VIEW,
+                      methods=['GET', ])
 
-QUESTION_APP.add_url_rule(
-    '/api/v1/questions/', view_func=QUESTION_VIEW, methods=['POST', ])

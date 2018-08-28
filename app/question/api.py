e720137a7f5af7356d630405d62ab2e3410af74f
Toskgreg <toskingregory@icloud.com>
2018-08-28 19:25:39 +0300

    [#160097665] adding post question methodview

--- app/question/api.py
+++ app/question/api.py
@@ -1,34 +1,43 @@
-"""This module handles QUESTIONAPI class and its methods"""
-import uuid
+"""Handles questions class based views"""
 from flask.views import MethodView
-from flask import jsonify, request, make_response
-from app.models import Question,QUESTIONS
+from flask import jsonify, request, make_response, current_app as app
+from app.models import Question
+from app.auth.decoractor import token_required
+from app.validate import validate_date, validate_question
+from app.database import Database, QuestionBbQueries
 
 
-class QUESTIONAPI(MethodView):
-    """This class based view handles question related methods"""
-    @staticmethod
-    def get(question_id):
-        """Method for  get requests"""
-        if question_id:
-            question_id = uuid.UUID(question_id)
-            question = Question.view_question_by_ID(question_id)
-            return jsonify(question), 200
-        else:
-            questions = Question.view_all_questions()
-            if QUESTIONS == []:
-                response = {
-                    "msg": " There are no questions at the moment"}
-                return make_response(jsonify(response)), 200
-            return jsonify(questions), 200
+class QuestionAPI(MethodView):
+    """A class based view to handle questions"""
+    decorators = [token_required]
 
-    @staticmethod
-    def post():
-        '''Method for a post request'''
-        data = request.json
-        question_title = data["question_title"]
-        question_description = data["question_description"]
-        res = Question.ask_question(question_title, question_description)
-        if res == "Your question has been successfully asked.":
-            return jsonify({'msg': res}), 201
-        return jsonify({'msg': res}), 201
+    def post(self, current_user):
+        """offers a new question"""
+        database = Database(app.config['DATABASE_URL'])
+        question_db = QuestionBbQueries()
+        data = request.get_json()
+
+        if validate_date(data['date']) != 'valid':
+            return jsonify({'message': validate_date(data['date'])}), 406
+
+        elif validate_question(data) == 'valid':
+
+            posted_by = current_user.username
+
+            questions_query = question_db.fetch_all()
+            for question in questions_query:
+                if question['title'] == data['title'] and \
+                        question['description1'] == data['description1']\
+                        and str(question['date']) == str(data['date']) and \
+                        question['posted_by'] == current_user.username:
+                    response = {
+                        'message': 'This question already exists.',
+                    }
+                    return make_response(jsonify(response)), 409
+            question_db.insert_question_data(data, posted_by)
+            response = {
+                'message': 'You offered a question successfully.',
+            }
+            return make_response(jsonify(response)), 201
+        return jsonify({'message': validate_question(data)}), 406
+

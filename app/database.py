f5999e9e936de1ca2a5063ffc21d3ead020b8d55
Toskgreg <toskingregory@icloud.com>
2018-08-28 20:10:15 +0300

    [#160097705] adding answer database queries class to database file

--- app/database.py
+++ app/database.py
@@ -35,8 +35,14 @@ class Database:
 
         create_table = "CREATE TABLE IF NOT EXISTS questions\
         (id SERIAL PRIMARY KEY, title text, description1\
-        text, date TIMESTAMP NOT NULL, posted_by text)"
+        text, date TIMESTAMP NOT NULL, qauthor text)"
         self.cur.execute(create_table)
+
+        create_table = "CREATE TABLE IF NOT EXISTS answers\
+        (id SERIAL PRIMARY KEY,aauthor text ,question_id INTEGER, status text,text1 text)"
+        self.cur.execute(create_table)
+        self.conn.commit()
+        self.conn.close()
 
 
     def trancate_table(self, table):
@@ -50,6 +56,14 @@ class Database:
         self.cur.execute(query)
         row = self.cur.fetchone()
         return row
+    def fetch_by_paramss(self, table_name, column, param):
+        """Fetches a single a parameter from a specific table and column"""
+        query = "DELETE FROM {} WHERE {} = '{}'".format(
+             table_name, column, param)
+        self.cur.execute(query)
+        row = self.cur.rowcount
+        return row
+
 
 class UserBbQueries(Database):
     """This class handles database transactions for the user"""
@@ -73,24 +87,84 @@ class QuestionBbQueries(Database):
     def __init__(self):
         Database.__init__(self, app.config['DATABASE_URL'])
 
-    def insert_question_data(self, data, posted_by):
+    def insert_question_data(self, data, qauthor):
         """Insert a new question record to the database"""
-        query = "INSERT INTO questions (title, description1, date, posted_by)\
+        query = "INSERT INTO questions (title, description1, date, qauthor)\
         VALUES('{}', '{}', '{}', '{}');".format(data['title'],
                                                 data['description1'],
-                                                data['date'], posted_by)
+                                                data['date'], qauthor)
         self.cur.execute(query)
         self.conn.commit()
 
     def fetch_all(self):
-        """ Fetches all question records from the database"""
+        """ Fetches all question recods from the database"""
         self.cur.execute("SELECT * FROM questions ")
         rows = self.cur.fetchall()
         questions = []
         for row in rows:
             row = {'id': row[0], 'title': row[1],
                    'description1': row[2],
-                   'date': row[3], "posted_by": row[4]
+                   'date': row[3], "qauthor": row[4]
                    }
             questions.append(row)
         return questions
+
+class AnswerBbQueries(Database):
+    """This class handles database transactions for answers"""
+
+    def __init__(self):
+        Database.__init__(self, app.config['DATABASE_URL'])
+        self.status = 'answered'
+
+    def post_answer(self, question_id,data ,aauthor):
+        query = "INSERT INTO answers (aauthor,question_id, status,text1)\
+            VALUES('{}','{}', '{}','{}');".format(aauthor,question_id, self.status,data['text1'])
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def fetch_by_id(self, question_id):
+        """ Gets a question by id from the answers table"""
+        self.cur.execute(
+            "SELECT * FROM answers WHERE question_id = '{}'".format(question_id))
+        rows = self.cur.fetchall()
+        answers = []
+        for row in rows:
+            row = {'id': row[0], 'question_id': row[1],
+                   'status': row[2], 'text1': row[3],'aauthor': row[4]
+                   }
+            answers.append(row)
+        return answers
+
+    def update_answer_status(self, question_id, data):
+        '''Updates the status in the database'''
+        self.cur.execute("UPDATE answers SET status='{}' WHERE id='{}'"
+                         .format(data['status'], question_id))
+        self.conn.commit()
+
+
+    def update_answer(self, answer_id, text1):
+        self.cur.execute("UPDATE answers SET text1='{}' WHERE id={}"
+                         .format(text1, answer_id))
+        self.conn.commit()
+
+    def fetch_by_arg(self, colomn, aauthor):
+        """ Gets a question by aauthor from the answers table"""
+        self.cur.execute(
+            "SELECT * FROM answers WHERE {} = '{}'"
+            .format(colomn, aauthor))
+        rows = self.cur.fetchall()
+
+        answers = []
+        for row in rows:
+            row = {'id': row[0], 'question_id': row[1],
+                   'status': row[2], 'text1': row[3],'aauthor': row[3]
+                   }
+            answers.append(row)
+        return answers
+
+
+    
+   
+
+
+

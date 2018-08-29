8013ffab599968d357d5cb372a71d26b41daae50
Toskgreg <toskingregory@icloud.com>
2018-08-29 20:09:31 +0300

    [#160096510]adding proc file to host on heroku.

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class TestingConfiguration(Config):
 class ProductionConfiguration(Config):
     """Configurations for Production."""
     DEBUG = False
+    DATABASE_URL = 'postgres://sxeryejnkxvdyl:fe0c442231427f6b1874f34a2608f09568dcbf48d4a65c22af4e5697a769761f@ec2-54-235-242-63.compute-1.amazonaws.com:5432/df6pburvodvkqn'
 
 
 app_config = {
